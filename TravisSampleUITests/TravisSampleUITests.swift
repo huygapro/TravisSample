383d3d3256d825196bac6c1e670fe9eac5fa6747
Huy Nguyen <conghuy2012@gmail.com>
2020-02-24 17:19:31 +0700

    Take snapshoot from fastlane

---
+++ TravisSampleUITests/TravisSampleUITests.swift
@@ -0,0 +1,48 @@
+//
+//  TravisSampleUITests.swift
+//  TravisSampleUITests
+//
+//  Created by Huy Nguyen on 2/24/20.
+//  Copyright © 2020 Nguyen Cong Huy. All rights reserved.
+//
+
+import XCTest
+
+class TravisSampleUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        
+        app.buttons["Button"].tap()
+        
+        snapshot("DetailSc")
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
